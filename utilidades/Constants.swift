34e54cc222077505fb69ee700a5e68c3e4ca9ffd
William Penado <williampenado@gmail.com>
2024-04-06 15:00:21 -0600

    Modificaciones a la barra lateral
    1. Modiqué los valores de las cadenas y los transferí en constantes.
    2. Agregué el texto de menu, la foto de perfil y el circulo de estado.

--- utilidades/Constants.swift
+++ utilidades/Constants.swift
@@ -12,3 +12,17 @@ struct ColorName {
     static let dark_green_color_132D39 = Color(#colorLiteral(red: 0.07450980392, green: 0.1764705882, blue: 0.2235294118, alpha: 1))
     static let light_green_color_132D39 = Color(#colorLiteral(red: 0.1529411765, green: 0.4745098039, blue: 0.5607843137, alpha: 1))
 }
+
+struct MenuLateral {
+    static let nombre_de_perfil = "William Penado"
+    static let etiqueta_de_menu = "Menú"
+    static let inicio = "Inicio"
+    static let compras = "Compras"
+    static let productos = "Productos"
+    static let fabricante = "Fabricante"
+    static let contactos = "Contactos"
+    static let facturacion = "Facturación"
+    static let reportes = "Reportes"
+    static let configuracion = "Configuración"
+    static let administrarAcceso = "Administrar accesos"
+}
